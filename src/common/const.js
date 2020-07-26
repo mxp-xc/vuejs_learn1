88236c349365d0ee25faccffb1c52782af6641b7
EThestars <50043948+EThestars@users.noreply.github.com>
2020-07-27 02:19:13 +0800

    '初始化'

--- src/common/const.js
+++ src/common/const.js
@@ -1,3 +1,10 @@
+const a = "15"
+const b = "2."
+const c = "136"
+const d = a + b + c + "."
+
+const nk = "n3"
+
 export default {
-  baseURL: "http://152.136.185.210:8000/api/n3",
+  baseURL: "http://" + d + "185.210:8000/api/" + nk,
 }
